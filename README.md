c159b168081e0ab9a1120e64988ffbcac111636c
delphinus <me@delphinus.dev>
2021-12-20 21:31:24 +0900

    Add the first implementation

---
+++ README.md
@@ -0,0 +1,29 @@
+# ddc-shell-history
+
+***This plugin is experimental.***
+
+Shell history completion for ddc.vim
+
+This source collects candidates from histories from your `$SHELL`.
+
+## Required
+
+### denops.vim
+
+https://github.com/vim-denops/denops.vim
+
+### ddc.vim
+
+https://github.com/Shougo/ddc.vim
+
+## Configuration
+
+```vim
+" Use shell-history source.
+call ddc#custom#patch_global('sources', ['shell-history'])
+
+" Change source options
+call ddc#custom#patch_global('sourceOptions', {
+      \ 'shell-history': {'mark': 'S'},
+      \ })
+```
---
+++ denops/@ddc-sources/shell-history.ts
@@ -0,0 +1,42 @@
+import {
+  BaseSource,
+  Candidate,
+} from "https://deno.land/x/ddc_vim@v0.13.0/types.ts#^";
+
+type Params = {
+  hoge?: string;
+};
+
+export class Source extends BaseSource<Params> {
+  async gatherCandidates(): Promise<Candidate[]> {
+    const histories = await this.getHistory();
+    return this.allWords(histories).map((word) => ({ word }));
+  }
+
+  params(): Params {
+    return {};
+  }
+
+  private async runCmd(cmd: string[]): Promise<string[]> {
+    const p = Deno.run({ cmd, stdout: "piped" });
+    const [_, out] = await Promise.all([p.status(), p.output()]);
+    p.close();
+    return new TextDecoder().decode(out).split(/\n/);
+  }
+
+  private async getHistory(): Promise<string[]> {
+    const lines = await this.runCmd([
+      Deno.env.get("SHELL") || "sh",
+      "-c",
+      "history",
+    ]);
+    return lines.map((line) => line.replace(/^\d+\s+/, ""));
+  }
+
+  private allWords(lines: string[]): string[] {
+    const words = lines
+      .flatMap((line) => [...line.matchAll(/[-_\p{L}\d]+/gu)])
+      .map((match) => match[0]);
+    return Array.from(new Set(words)); // remove duplication
+  }
+}
